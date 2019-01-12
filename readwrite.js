cd87a4e65d237a15d18a46c0920ff4b59b80cad3
Harry Park <hg98.park@hanwha.com>
2019-01-12 20:59:06 +0900

    #1 createReadStream()

--- readwrite.js
+++ readwrite.js
@@ -1,7 +1,9 @@
-const fs = require('fs');
-fs.readFile('target.txt', function(err, data) {
-    if (err) {
-        throw err;
-    }
-    console.log(data.toString());
+const 
+    fs = require('fs')
+    stream = fs.createReadStream(process.argv[2]);
+stream.on('data', function  (chunk) {
+    process.stdout.write(chunk);
 });
+stream.on('error', function  (err) {
+    process.stderr.write("ERROR: " + err.message + "\n");
+})
