4eb17ec16d5baeb725a66d9c3848eb78075faa42
Harry Park <hg98.park@hanwha.com>
2019-01-12 20:39:40 +0900

    #1 event emitter

--- watcher.js
+++ watcher.js
@@ -9,7 +9,14 @@ if (!filename) {
     throw Error("A file to watch must be specified!") ;
 }
 fs.watch('target.txt', function( ) {
-    let ls = spawn('cmd', ['/c', 'dir', filename]);
-    ls.stdout.pipe(process.stdout);
+    let ls = spawn('cmd', ['/c', 'dir', '/B', filename]);
+    let output = '';
+    ls.stdout.on('data', function(chunk) {
+        output += chunk.toString();
+    });
+    ls.on('close', function() {
+        let parts = output.split(/ls+/);
+        console.dir([parts[0], parts[4]]);
+    })
 }) ;
 console.log("Now watching target.txt for changes...");
