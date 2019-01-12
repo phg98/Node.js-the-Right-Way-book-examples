a78845237c21e440ae6740156cca6700787dab32
Harry Park <hg98.park@hanwha.com>
2019-01-12 18:52:35 +0900

    #1 spawn, windows command, stream

--- watcher.js
+++ watcher.js
@@ -1,11 +1,15 @@
+// Example code for the book "Node.js the Right Way"
+"use strict" ;
 const 
-    fs = require ('fs')
+    fs = require ('fs'),
+    spawn = require('child_process').spawn,
     filename = process.argv[2] ;
 
 if (!filename) {
     throw Error("A file to watch must be specified!") ;
 }
 fs.watch('target.txt', function( ) {
-    console.log("File " + filename + " just changed!") ;
+    let ls = spawn('cmd', ['/c', 'dir', filename]);
+    ls.stdout.pipe(process.stdout);
 }) ;
 console.log("Now watching target.txt for changes...");
