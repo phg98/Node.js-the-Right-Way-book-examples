697f528887eabb6ef18a2e89e563e93b49fa61df
Harry Park <hg98.park@hanwha.com>
2019-01-12 13:13:33 +0900

    #1 throw, argv

--- watcher.js
+++ watcher.js
@@ -1,5 +1,11 @@
-const fs = require ('fs');
+const 
+    fs = require ('fs')
+    filename = process.argv[2] ;
+
+if (!filename) {
+    throw Error("A file to watch must be specified!") ;
+}
 fs.watch('target.txt', function( ) {
-    console.log("File 'target.txt' just changed!") ;
+    console.log("File " + filename + " just changed!") ;
 }) ;
 console.log("Now watching target.txt for changes...");
