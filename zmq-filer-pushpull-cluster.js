09a5244cb517cb41bb8ba38fa6b57f2e5059ca8e
phg98 <phg98@users.noreply.github.com>
2019-01-16 21:58:10 +0900

    Resolves #4 update push-pull example code with authors code.

--- zmq-filer-pushpull-cluster.js
+++ zmq-filer-pushpull-cluster.js
@@ -1,58 +1,88 @@
-'use strict';
+/***
+ * Excerpted from "Node.js the Right Way",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/jwnode for more book information.
+***/
+"use strict";
 const
-    cluster = require('cluster'),
-    fs = require('fs'),
-    zmq = require('zmq');
+  cluster = require('cluster'),
+  zmq = require('zmq');
 
-if(cluster.isMaster) {
-    let 
-        online_worker = 0, ready_count = 0,
-        pusher = zmq.socket('push').bind('tcp://127.0.0.1:5433'),
-        puller = zmq.socket('pull').bind('tcp://127.0.0.1:5434');
-
-    puller.on('message', function(){
-        let frames = Array.prototype.slice.call(arguments);
-        if (ready) {
-            ready_count++;
-            if (ready_count>=3) {
-                pusher.send(JSON.stringify({
-                    path: "test.txt"
-                }));
-            }
-        } else if (result_message) {
-            console.log('result_message : ' + result_message);
-        }
-    });
-
-    cluster.on('online', function(worker) {
-        online_worker++;
-        console.log('Worker ' + worker.process.pid + ' is online. total workers ' + online_worker);
-    });
-
-    for (let i=0; i<3; i++) {
-        cluster.fork();
-    }
-} else {
-    let puller = zmq.socket('pull').connect('tcp://127.0.0.1:5433');
-    let pusher = zmq.socket('push').connect('tcp://127.0.0.1:5434');
-    puller.on('message', function(data) {
-        let request = JSON.parse(data);
-        console.log(process.pid + ' Received request to get: ' + request.path);
+if (cluster.isMaster) {
+  
+  // master process
+  // - create PUSH and SUB sockets, bind IPC endpoints
+  let
+    pusher = zmq.socket('push').bind('ipc://filer-pusher.ipc'),
+    puller = zmq.socket('pull').bind('ipc://filer-puller.ipc'),
     
-        // read file and reply with content
-        fs.readFile(request.path, function(err, data){
-            //if (err) Error(err);
-            console.log(process.pid + ' Sending response content' + data);
-            pusher.send(JSON.stringify({
-                type: "result",
-                data: data.toString(),
-                timestamp: Date.now(),
-                pid: process.pid
-            }));
-        });
-    });
+    readyCount = 0,
+    
+    sendWork = function(){
+      // all workers are ready, send out thirty jobs
+      for (let i = 0; i < 30; i++) {
+        pusher.send(JSON.stringify({index: i}));
+      }
+    };
+  
+  // listen for worker messages
+  puller.on('message', function(data) {
+    
+    let message = JSON.parse(data);
+    
+    if (message.ready) {
+      readyCount += 1;
+      if (readyCount === 3) {
+        sendWork();
+      }
+    } else if (message.result) {
+      console.log('received: ' + data); 
+    }
+    
+  });
+  
+  // fork three worker processes
+  for (let i = 0; i < 3; i++) {
+    cluster.fork();
+  }
+  
+  // listen for workers to come online
+  cluster.on('online', function(worker) {
+    console.log('Worker ' + worker.process.pid + ' is online.');
+  });
+  
+} else {
+  
+  // worker process
+  // - create PULL socket, connect to master's PUSH
+  // - create PUB socket, connect to master's SUB
+  let
+    puller = zmq.socket('pull').connect("ipc://filer-pusher.ipc"),
+    pusher = zmq.socket('push').connect("ipc://filer-puller.ipc");
+  
+  puller.on('message', function(data) {
+    
+    // parse incoming message
+    let job = JSON.parse(data);
+    console.log(process.pid + " received job: " + job.index);
+    
+    // publish response
     pusher.send(JSON.stringify({
-        type: "ready"
-    }))
+      index: job.index,
+      pid: process.pid,
+      result: 'success'
+    }));
+    
+  });
+  
+  // signal ready
+  pusher.send(JSON.stringify({
+    ready: true,
+    pid: process.pid
+  }));
+  
 }
 
